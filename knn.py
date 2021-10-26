dc5747e39142494991fc7a45654b29c33010b87a
Aidan Barbieux wsl <abarbieu@calpoly.edu>
2021-10-26 16:15:32 -0700

    cleaned knn, ran on data

--- knn.py
+++ knn.py
@@ -11,31 +11,6 @@ from collections import Counter
 from classifier import initializeConfusion
 
 
-# In[51]:
-
-
-def readCommandLine(file=None, k=None):
-    if len(sys.argv) < 3:
-        print("Usage: python3 knn.py <filename> <k>")
-        exit(1)
-    
-    if file == None:
-        file = sys.argv[1]
-    if k == None:
-        k = int(sys.argv[2])
-        
-    if k <= 1:
-        print("K has to be greater than 1")
-        exit(1)
-        
-    df = pd.read_csv(file)
-    df = prepareData(df)
-    return df, k, attrs
-
-
-# In[50]:
-
-
 # normalizes all the numeric columns
 def normalizeNumeric(df, attrs):
     for a in attrs:
@@ -53,6 +28,11 @@ def normalizeNumeric(df, attrs):
 
 
 def prepareData(df):
+    attrs = {}
+    for a in df.columns:
+        attrs[a] = int(df[a][0])
+    attrs.pop(df.columns[-1])
+    
     aclass = df.iloc[1,0]
     
     # get attributes and whether or not they are numeric
@@ -63,21 +43,24 @@ def prepareData(df):
     # drop the numeric and class label rows
     df = df.drop([0, 1], axis=0)
     
+    # clean data and convert to numeric
+    for a in attrs:
+        if attrs[a] == 0:
+            df[a] = pd.to_numeric(df[a], errors='coerce')
+    df = df.dropna()
+    df = df[(df != '?').all(axis=1)]
+    
     # getting list of categorical columns to pass to get_dummies to binarize categorical data
     catCols = [col for col in attrs if attrs[col] >= 1 and col != aclass]
     df = pd.get_dummies(df, columns = catCols)
     
     # append class labels to end of dataframe
     df = df[[c for c in df if c not in [aclass]] + [aclass]]
-    
-    # converting numeric data to non-string types
-    for a in attrs:
-        if attrs[a] < 1:
-            df[a] = pd.to_numeric(df[a])
-    
+        
     # normalizing the numeric columns
     df = normalizeNumeric(df, attrs)
-    return df
+   
+    return df, attrs
 
 
 # In[99]:
@@ -145,10 +128,24 @@ def knnPredictions(data, k):
     accuracy = getStats(confusion, predictions)
     print(f"Accuracy of KNN with k = {k}: {accuracy:.4f}")
     print("Confusion Matrix")
+    print("Predicted \u2193, Actual \u2192")
     print(confusion)
     
 # with heart.csv, k = 2 gives the best accuracy with 0.7026
 if __name__ == "__main__":
-    df, k, tmp = readCommandLine("./data/heart.csv", 2)
+    if len(sys.argv) == 3:
+        _, datafile, k = sys.argv
+    else:
+        print("Usage: python3 knn.py <filename> <k>")
+        exit(1)
+    k = int(k)
+        
+    if k <= 1:
+        print("K has to be greater than 1")
+        exit(1)
+        
+    df = pd.read_csv(datafile)
+    df, attrs = prepareData(df)
+
     knnPredictions(df, k)
 
